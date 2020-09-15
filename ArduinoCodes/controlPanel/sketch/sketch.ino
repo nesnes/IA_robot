1342261b7063aa264a37021a4b3aabed9efede9d
nesnes <alex-nesnes@hotmail.fr>
2020-09-16 00:18:45 +0200

    update control Panel

--- ArduinoCodes/controlPanel/sketch/sketch.ino
+++ ArduinoCodes/controlPanel/sketch/sketch.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <Wire.h>
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -11,90 +12,49 @@
 
 #define DISPLAY_ON
 #ifdef DISPLAY_ON
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+Adafruit_SSD1306 display(128,64,OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #endif
 
 #define START_BTN_PIN 7
 #define COLOR_BTN_PIN 8
 
+#define MODE_I2C
+#ifndef MODE_I2C
+  #define MODE_SERIAL
+  //#define SERIAL_DEBUG
+#endif
+#include "comunication.h"
+
 static const unsigned char PROGMEM logo16_ARI[] =
 { B00000001, B10000000,
   B00000111, B11100000,
-  B00011100, B00111000,
+  B00011000, B00111000,
   B00100000, B00000100,
-  B00100100, B00100100,
   B00100000, B00000100,
-  B00011111, B11111000,
-  B00000000, B00000000,
-  B00111111, B11111100,
-  B00111000, B00011100,
-  B00001000, B00010000,
-  B00001000, B00010000,
-  B00100001, B10000111,
-  B01010001, B01000010,
-  B11111001, B10000010,
-  B10001001, B01000111 };
+  B00111100, B11100100,
+  B00100011, B00010100,
+  B00101011, B01010100,
+  B00100011, B00010100,
+  B00111100, B11100100,
+  B00100000, B00000100,
+  B00100000, B00000100,
+  B00101111, B11100100,
+  B00110101, B01010100,
+  B00110101, B01010100,
+  B00101111, B11100100 };
 
 #if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
 #if(SERIAL_TX_BUFFER_SIZE == 64)
-#error("Reduce the buffer sizes to RX=32 TX=32 in the #defines of \\Arduino-1.x.x\\hardware\\arduino\\cores\\arduino\\HardwareSerial.h")
+//#error("Reduce the buffer sizes to RX=32 TX=32 in the #defines of \\Arduino-1.x.x\\hardware\\arduino\\cores\\arduino\\HardwareSerial.h")
 #endif
 
 int freeRam () {
   extern int __heap_start, *__brkval; 
   int v; 
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
-}
-
-#define LINE_CHAR_SIZE 23
-char messages[7][LINE_CHAR_SIZE];
-int messageIndex = -1;
-
-void scrollTextUp(){
-  for( int i=1;i<7;i++){
-    memcpy(messages[i-1], messages[i], LINE_CHAR_SIZE);
-  }
-  memset(messages[6], '\0', LINE_CHAR_SIZE);
-}
-
-//Very important to add a '\0' at the end of the char* message
-void addMessage(const char* msg){
-  messageIndex++;
-  if(messageIndex == 7){
-    messageIndex = 6;
-    scrollTextUp();
-  }
-  memset(messages[messageIndex], '\0', LINE_CHAR_SIZE);
-  int rowSize = LINE_CHAR_SIZE - 2;
-  if(messageIndex==0)
-    rowSize = 18;
-  int msgLength = 0;
-  for(int i=0;msg[i]!='\0';i++)
-    msgLength++;
-  int charNumber = min(rowSize, msgLength);
-  for(int i=0;i<charNumber;i++)
-    messages[messageIndex][i] = msg[i];
-  messages[messageIndex][rowSize+1] = '\0';
-  
-  if(msgLength>rowSize){
-    char subMsg[msgLength-charNumber+1];
-    for(int i=charNumber;i<msgLength;i++)
-      subMsg[i-charNumber] = msg[i];
-    subMsg[msgLength-charNumber] = '\0';
-    addMessage(subMsg);
-  }
-}
-
-void printMessages(){
-  #ifdef DISPLAY_ON
-  display.setCursor(0, 9);
-  for( int i=0;i<7;i++){
-    display.println(messages[i]);
-  }
-  #endif
 }
 
 int getColor(){
@@ -113,14 +73,18 @@ int getStart(){
 
 int colorStatus = 0;
 int startStatus = 0;
-int score = 42;
+int score = 0;
 void printButtons(){
   #ifdef DISPLAY_ON
   display.setCursor(0, 0);
+  display.setTextSize(1);
   char line[19];
   sprintf(line, "C=%i S=%i Score=%i\0", colorStatus, startStatus, score);
   display.println(line);
   display.drawFastHLine(0, 7, 110, 1);
+  display.setTextSize(7);
+  display.setCursor(2, 17);
+  display.println(score);
   #endif
 }
 
@@ -128,15 +92,15 @@ void updateScreen(){
   #ifdef DISPLAY_ON
   display.clearDisplay();
   printButtons();
-  printMessages();
   display.fillRect(110, 0, 17, 16, BLACK);
   display.drawBitmap(111, 0, logo16_ARI, 16, 16, WHITE);
   display.display();
   #endif
 }
 
-void setup()   {                
-  Serial.begin(115200);
+void setup()   {          
+  comunication_begin(9);//I2C address 9      
+  //Serial.begin(115200);
   pinMode(START_BTN_PIN, INPUT_PULLUP);
   pinMode(COLOR_BTN_PIN, INPUT_PULLUP);
   #ifdef DISPLAY_ON
@@ -146,69 +110,43 @@ void setup()   {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   #endif
-  for( int i=0;i<7;i++){
-      memset(messages[i], '\0', LINE_CHAR_SIZE);
-  }
   startStatus = getStart();
   colorStatus = getColor();
   updateScreen();
 }
 
-#define INPUT_STR_SIZE 32
-#define OUPUT_STR_SIZE 32
-char inputString[INPUT_STR_SIZE];
-char outputString[OUPUT_STR_SIZE];
-boolean stringReady = false;
-
-void readSerial() {
-  int i=0;
-  while (Serial.available()) {
-    delay(3);
-    char inChar = (char)Serial.read();
-    if (inChar == '\r' || inChar == '\n' || i>INPUT_STR_SIZE-2) {
-      break;
+void executeOrder(){
+  comunication_read();
+  if(comunication_msgAvailable()){
+    if(comunication_InBuffer[0] == '#' && comunication_InBuffer[1] != '\0'){
+      //ignore
     }
-    inputString[i++] = inChar;
+    else if(!strcmp(comunication_InBuffer, "id")){
+      sprintf(comunication_OutBuffer, "ControlPanel");//max 29 Bytes
+      comunication_write();//async
+    }
+    else if(strstr(comunication_InBuffer, "score set")){
+      sscanf(comunication_InBuffer, "score set %i ", &score);
+      sprintf(comunication_OutBuffer, "OK");//max 29 Bytes
+      comunication_write();//async
+    }
+    else if(!strcmp(comunication_InBuffer, "get")){
+      sprintf(comunication_OutBuffer, "C %i S %i", colorStatus, startStatus);//max 29 Bytes
+      comunication_write();//async
+    }
+    else{
+      sprintf(comunication_OutBuffer,"ERROR");
+      comunication_write();//async
+    }
+    comunication_cleanInputs();
   }
-  inputString[i+1] = '\0';
-  stringReady = i > 0;
-}
-
-void resetOutputString(){
-  memset(outputString, '\0', OUPUT_STR_SIZE);
-}
+}   
 
 void loop() {
   int lastScore = score;
-  readSerial();
-  if(stringReady){
-    if(inputString[0] == '#' && inputString[1] != '\0'){ //Print debug message
-      addMessage(inputString+1); //+1 on the pointer to move after the #
-      updateScreen();
-    }
-    else if(!strcmp(inputString, "color get")){
-      sprintf(outputString, "color %i\r\n", colorStatus);
-      Serial.print(outputString);
-    }
-    else if(!strcmp(inputString, "start get")){
-      sprintf(outputString, "start %i\r\n", startStatus);
-      Serial.print(outputString);
-    }
-    else if(strstr(inputString, "score set") != NULL){
-      sscanf(inputString, "score set %i", &score);
-    }
-    else if(!strcmp(inputString, "id")){
-      sprintf(outputString, "ControlPanelAlexV1\r\n"); //max 32 bit (with \r\n)
-      Serial.print(outputString);
-    }
-    else{
-      Serial.print("ERROR\r\n");
-    }
-    memset(inputString, '\0', INPUT_STR_SIZE);
-    stringReady = false;
-  }
   int color = getColor();
   int start = getStart();
+  executeOrder();
   if(color != colorStatus || start != startStatus || lastScore != score){
     colorStatus = color;
     startStatus = start;
